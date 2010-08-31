1a01d07c35f13decd05b00814624c01cbb76e955
rogerta@google.com <rogerta@google.com@10bc0f33-e4bf-9a86-80cf-af638054f0c4>
2010-08-31 14:15:13 +0000

    Add the forth argument to RegKey::ReadValue calls in preparation for a change in the base/registry.h API.
    The default argument is going to be removed, so in order to make rlz compiled
    under chrome we need to add NULL in the last argument.
    
    BUG=None
    TEST=compiles
    
    Patch from Thiago Farina <tfarina@chromium.org>
    
    Review URL: http://codereview.appspot.com/2085041

--- win/lib/machine_deal.cc
+++ win/lib/machine_deal.cc
@@ -291,7 +291,7 @@ bool MachineDealCode::Clear() {
   // Verify deletion.
   wchar_t dcc[kMaxDccLength + 1];
   DWORD dcc_size = arraysize(dcc);
-  if (dcc_key.ReadValue(kDccValueName, dcc, &dcc_size)) {
+  if (dcc_key.ReadValue(kDccValueName, dcc, &dcc_size, NULL)) {
     ASSERT_STRING("MachineDealCode::Clear: Could not delete the DCC value.");
     return false;
   }
