e69b9566206b91d08af3de53709bd916eca70101
rogerta@google.com <rogerta@google.com@10bc0f33-e4bf-9a86-80cf-af638054f0c4>
2010-10-07 16:05:19 +0000

    Add a test to verify that the machine id algorithm does not in advertently change, generating a different id for the same machine.
    Review URL: http://codereview.appspot.com/2367042

--- win/lib/machine_deal.h
+++ win/lib/machine_deal.h
@@ -50,6 +50,11 @@ class MachineDealCode {
   // machine SID plus a checksum byte.
   static bool GetMachineId(std::wstring* id);
 
+  // Calculates the universal ID for a machine given an sid and volume id.
+  static bool GetMachineIdImpl(const std::wstring& sid_string,
+                               int volume_id,
+                               std::wstring* id);
+
  protected:
   // Clear the DCC value. Only for testing purposes.
   // Requires write access to HKLM, unless rlz_lib::CreateMachineState() has
